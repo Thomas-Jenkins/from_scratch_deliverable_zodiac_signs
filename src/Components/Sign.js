aad0a1e70b724dfaca52bf5ec831e3a716e83d43
Thomas Jenkins <Jenkinsth@yahoo.com>
2022-09-13 15:27:09 -0700

    fixed issue with images not rendering to screen

--- src/Components/Sign.js
+++ src/Components/Sign.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Sign({ name, dates }) {
   return <>
     <div>
-      <img alt={name} src={`${process.env.PUBLIC_URL}/assets/${name}.png`} />
-      <span>{name}</span>
-      <span>{dates}</span>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/signimg/${name}.png`} />
+      <p>{name}</p>
+      <p>{dates}</p>
     </div>  
   </>;
 }
